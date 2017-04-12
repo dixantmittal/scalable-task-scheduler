e84af1af9776e022887b3dd95a5b6bcb235ecd27
dixant <dixant@ixigo.com>
2017-04-12 14:41:51 +0530

    > removes useless file. > uses concurrentHashmap instead of HashMap

--- KafkaQueuingService/src/main/java/com/ixigo/factory/KafkaProducerFactory.java
+++ KafkaQueuingService/src/main/java/com/ixigo/factory/KafkaProducerFactory.java
@@ -4,8 +4,8 @@ import com.ixigo.client.entity.IxigoKafkaProducer;
 import com.ixigo.entity.KafkaTopic;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by dixant on 22/03/17.
@@ -13,14 +13,10 @@ import java.util.Map;
 @Component
 public class KafkaProducerFactory {
 
-    private Map<KafkaTopic, IxigoKafkaProducer> _PRODUCER_CACHE = new HashMap<KafkaTopic, IxigoKafkaProducer>();
+    private Map<KafkaTopic, IxigoKafkaProducer> _PRODUCER_CACHE = new ConcurrentHashMap<KafkaTopic, IxigoKafkaProducer>();
 
     public IxigoKafkaProducer getKafkaProducer(KafkaTopic topic) {
-        if (_PRODUCER_CACHE.get(topic) == null) {
-            synchronized (this.getClass()) {
-                _PRODUCER_CACHE.putIfAbsent(topic, IxigoKafkaProducerBuilder.buildNewKafkaProducerWithTopic(topic));
-            }
-        }
+        _PRODUCER_CACHE.putIfAbsent(topic, IxigoKafkaProducerBuilder.buildNewKafkaProducerWithTopic(topic));
         return _PRODUCER_CACHE.get(topic);
     }
 
